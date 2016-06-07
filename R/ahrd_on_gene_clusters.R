26ee57dd9ab6721ac9b8122317aab02347ca21df
Dr. Asis Hallab <hallab@mpipz.mpg.de>
2016-06-07 11:28:48 +0200

    Recommending usage of mclapply for parsing InterPro database XML files

--- R/ahrd_on_gene_clusters.R
+++ R/ahrd_on_gene_clusters.R
@@ -6,39 +6,47 @@
 #'
 #' @return A list with three attributes 'ID', 'PARENTS', and 'CONTAINS'.
 #' @export
-parseInterProTag <- function(xml.node, include.abstracts=TRUE) {
+parseInterProTag <- function(xml.node, include.abstracts = TRUE) {
     ipr.id <- xmlGetAttr(xml.node, "id")
     ipr.prnts <- as.character(lapply(getNodeSet(xml.node, "./parent_list/rel_ref"), 
         function(x) xmlGetAttr(x, "ipr_ref")))
     ipr.contains <- as.character(lapply(getNodeSet(xml.node, "./contains/rel_ref"), 
         function(x) xmlGetAttr(x, "ipr_ref")))
-    ipr.name <- as.character(lapply(getNodeSet(xml.node, "./name"),
-        function(x) xmlValue(x, recursive=FALSE, trim=TRUE)))
+    ipr.name <- as.character(lapply(getNodeSet(xml.node, "./name"), function(x) xmlValue(x, 
+        recursive = FALSE, trim = TRUE)))
     ipr.tag <- list(ID = ipr.id, PARENTS = ipr.prnts, CONTAINS = ipr.contains, TYPE = xmlGetAttr(xml.node, 
-        "type"), SHORT.NAME = xmlGetAttr(xml.node, "short_name"), NAME=ipr.name)
-    if ( include.abstracts ) {
-      ipr.tag[['ABSTRACT']] <- as.character(lapply(getNodeSet(xml.node, "./abstract"),
-          function(x) xmlValue(x, trim=TRUE)))
+        "type"), SHORT.NAME = xmlGetAttr(xml.node, "short_name"), NAME = ipr.name)
+    if (include.abstracts) {
+        ipr.tag[["ABSTRACT"]] <- as.character(lapply(getNodeSet(xml.node, "./abstract"), 
+            function(x) xmlValue(x, trim = TRUE)))
     }
     ipr.tag
-} 
+}
 
-#' Read and parse the InterPro XML database. 
+#' Read and parse the InterPro XML database. If you have parallel installed and
+#' sufficient cores, it is highly recommended to use as many cores as you have
+#' to speed up parsing of the InterPro database. Also, save it for later use,
+#' so it does not have to be parsed again. See example for details.
 #'
 #' @param  path.2.xml The file path to the interpro.xml document
 #' @param  include.abstracts If TRUE the quite long abstract texts will be
 #' added to the result.                                      
 #'
 #' @return A named list of lists, each of which is the result of parsing a
-#' single InterPro XML node with function parseInterProTag(...).
+#' single InterPro XML node with function \code{parseInterProTag(...)}.
+#' @examples 
+#' options("mc.cores"=detectCores())
+#' ipr.db <- parseInterProXML( "./interpro.xml" )
+#' save( ipr.db, file="ipr_db.RData" )
+#'
 #' @export
-parseInterProXML <- function(path.2.xml, include.abstracts=TRUE) {
-    ipr.db <- lapply(getNodeSet(xmlInternalTreeParse(path.2.xml), "//interpro"), 
-        parseInterProTag, include.abstracts=include.abstracts)
+parseInterProXML <- function(path.2.xml, include.abstracts = TRUE) {
+    ipr.db <- mclapply(getNodeSet(xmlInternalTreeParse(path.2.xml), "//interpro"), 
+        parseInterProTag, include.abstracts = include.abstracts)
     names(ipr.db) <- as.character(mclapply(ipr.db, function(x) x$ID))
     # return
     ipr.db
-} 
+}
 
 #' Recursively traverses InterPro entries held in argument 'interpro.database'
 #' and finds all entries related to argument 'ipr.id' through relation
@@ -64,7 +72,7 @@ interProRecursion <- function(ipr.id, relship.slot = "PARENTS", interpro.databas
                 interpro.database = interpro.database)), ipr.lst[[relship.slot]])
         }
     }
-} 
+}
 
 #' Reduce a set of InterPro accessions (IDs) to those not being contained by
 #' others, and those not being parents of others. Hence give the 'most
@@ -75,20 +83,20 @@ interProRecursion <- function(ipr.id, relship.slot = "PARENTS", interpro.databas
 #' @return The character vector of InterPro accessions, that is the subset of
 #' argument 'irp.ids' filtered as descibed above.
 #' @export
-filterInterProClusterAnnotations <- function( ipr.ids, interpro.database=ipr.db ) {
-  ipr.discard <- unique(unlist(lapply(ipr.ids, function(x) union(interProRecursion(x, 
-      relship.slot = "PARENTS", interpro.database = interpro.database), interProRecursion(x, 
-      relship.slot = "CONTAINS", interpro.database = interpro.database)))))
-  setdiff(ipr.ids, ipr.discard) 
+filterInterProClusterAnnotations <- function(ipr.ids, interpro.database = ipr.db) {
+    ipr.discard <- unique(unlist(lapply(ipr.ids, function(x) union(interProRecursion(x, 
+        relship.slot = "PARENTS", interpro.database = interpro.database), interProRecursion(x, 
+        relship.slot = "CONTAINS", interpro.database = interpro.database)))))
+    setdiff(ipr.ids, ipr.discard)
 }
 
 #' Each gene family (cluster of amino acid sequences) has members that in turn
 #' might have InterPro annotations in argument 'ipr.annos'. Those are filtered
 #' in order to retain only those that are NOT contained in others and those
-#' that are NOT parents of others. Here, "others" refers to InterPro
+#' that are NOT parents of others. Here, 'others' refers to InterPro
 #' annotations found for genes of the cluster of course. The family 'fam' is
 #' then assigned the most frequent InterPro family(s), if their respective
-#' frequency is at least 0.5. If no such "frequent" InterPro family annotation
+#' frequency is at least 0.5. If no such 'frequent' InterPro family annotation
 #' is found, the most frequent InterPro annotation regardless of its type are
 #' used. In most cases these are of type domain.
 #'
@@ -97,7 +105,7 @@ filterInterProClusterAnnotations <- function( ipr.ids, interpro.database=ipr.db 
 #' holding the gene accessions, and the second holding the annotated InterPro
 #' entry
 #' @param interpro.databas the database of InterPro entries as parsed from the
-#' interpro XML document "interpro.xml". The format is a named list of named
+#' interpro XML document 'interpro.xml'. The format is a named list of named
 #' lists. See parseInterProXML(...) for more details.
 #'
 #' @return A list of InterPro accessions to be interpreted as the argument gene
@@ -121,5 +129,5 @@ annotateCluster <- function(fam, ipr.annos, interpro.database) {
             names(ipr.freqs[which(ipr.freqs == max.ipr.freq)])
         }
         list(most.frequent.IPRs = interpro.database[iprs], frequency = max(smr, na.rm = TRUE)/length(fam))
-    } 
+    }
 } 
