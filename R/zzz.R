f47a281f8a389b12492acef68b10629598ee4b55
Dr. Asis Hallab <hallab@mpipz.mpg.de>
2016-06-07 16:11:37 +0200

    Added warning message after loading

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 # Function ensured data-sets to be loaded, whenever this package is loaded.
 .onLoad <- function( libname = find.package( "AHRD.on.gene.clusters" ), pkgname = "AHRD.on.gene.clusters" ) {
     data( "ipr_and_families", package = "AHRD.on.gene.clusters" )
+    message("\nMAKE SURE THAT YOUR PROTEIN-INTERPRO-ANNOTATIONS ARE UNIQUE BEFORE YOU USE AHRD.on.gene.clusters\n")
 }
